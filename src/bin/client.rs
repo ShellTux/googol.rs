88e5eaae4a78adbb0d746deca68a64be18a68f5e
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-06-08 20:58:43 +0100

    refactor(client): rewrite cli args using clap derive

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+use clap::{Parser, Subcommand};
 use googol::{
     debugv,
     proto::{
@@ -8,9 +8,78 @@ use googol::{
     settings::{GoogolConfig, Load, client::ClientConfig},
 };
 use log::{debug, error};
-use std::{net::SocketAddr, str::FromStr, time::Duration};
+use std::{net::SocketAddr, time::Duration};
 use tokio::time::sleep;
 use tonic::{Request, Status, transport::Channel};
+use url::Url;
+
+#[derive(Debug, Parser)]
+#[command(
+    version,
+    about = "Client to interact with GatewayService Server",
+    version = "1.0",
+    author = "Luís Góis"
+)]
+struct Cli {
+    /// Gateway server address (e.g., 127.0.0.1:8080)
+    #[arg(short, long, help = "Gateway server address")]
+    address: Option<SocketAddr>,
+
+    /// Number of retries for exponential backoff when connecting via gRPC
+    #[arg(
+        short,
+        long,
+        help = "Number of retries for exponential backoff when connecting via gRPC"
+    )]
+    retries: Option<usize>,
+
+    /// Subcommands for specific operations
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Enqueue a URL for processing
+    Enqueue {
+        /// URL to enqueue
+        url: Url,
+    },
+
+    /// Search for provided words
+    Search {
+        /// Words to search for
+        #[arg(required = true)]
+        words: Vec<String>,
+    },
+
+    /// Consult backlinks or outlinks of a given page
+    Consult {
+        #[command(subcommand)]
+        consult_command: ConsultCommand,
+    },
+
+    /// Get real-time status of the system
+    RealTimeStatus,
+
+    /// Perform a health check
+    Health,
+}
+
+#[derive(Debug, Subcommand)]
+enum ConsultCommand {
+    /// Get backlinks pointing to the specified URL
+    Backlinks {
+        /// The URL to check for backlinks
+        url: Url,
+    },
+
+    /// Get outlinks from the specified URL
+    Outlinks {
+        /// The URL to check for outlinks
+        url: Url,
+    },
+}
 
 async fn connect_with_backoff<ClientType, F, Fut>(
     max_retries: usize,
@@ -54,6 +123,9 @@ where
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
+    let cli = Cli::parse();
+    debugv!(&cli);
+
     let settings = match GoogolConfig::default() {
         Err(e) => {
             error!("{:#?}", e);
@@ -65,116 +137,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     debugv!(settings);
 
-    let default_address = Box::leak(Box::new(format!("{}", settings.gateway)));
-    let default_max_retries = Box::leak(Box::new(settings.max_retries.to_string()));
+    let retries = cli.retries.unwrap_or(settings.max_retries);
+    let address = cli.address.unwrap_or(settings.gateway);
 
-    let cli = Command::new("Gateway Client")
-        .version("1.0")
-        .author("Luís Góis")
-        .about("CLI to interact with GatewayService")
-        .arg(
-            Arg::new("address")
-                .short('a')
-                .long("address")
-                .value_name("ADDRESS")
-                .help("Server address, e.g., 127.0.0.1:50051")
-                .default_value(default_address.as_str()),
-        )
-        .arg(
-            Arg::new("retries")
-                .short('r')
-                .long("retries")
-                .value_name("RETRIES")
-                .help("Number of retries for exponential backoff when connecting via gRPC, e.g.: 5")
-                .default_value(default_max_retries.as_str()),
-        )
-        .subcommand(
-            Command::new("enqueue").about("Enqueue a URL").arg(
-                Arg::new("url")
-                    .short('u')
-                    .long("url")
-                    .value_name("URL")
-                    .help("URL to enqueue")
-                    .required(true),
-            ),
-        )
-        .subcommand(
-            Command::new("search").about("Search pages by words").arg(
-                Arg::new("words")
-                    .short('w')
-                    .long("words")
-                    .value_name("WORDS")
-                    .help("Words to search for")
-                    .required(true),
-            ),
-        )
-        .subcommand(
-            Command::new("consult")
-                .about("Consult backlinks or outlinks of a given page")
-                .subcommand(
-                    Command::new("backlinks")
-                        .about("Consult backlinks of a given page")
-                        .arg(
-                            Arg::new("url")
-                                .short('u')
-                                .long("url")
-                                .value_name("url")
-                                .help("url to consult backlinks of")
-                                .required(true),
-                        ),
-                )
-                .subcommand(
-                    Command::new("outlinks")
-                        .about("Consult outlinks of a given page")
-                        .arg(
-                            Arg::new("url")
-                                .short('u')
-                                .long("url")
-                                .value_name("url")
-                                .help("url to consult backlinks of")
-                                .required(true),
-                        ),
-                ),
-        )
-        .subcommand(Command::new("real-time-status").about("Real Time Status of the system"))
-        .subcommand(Command::new("health").about("Perform health check"));
-
-    let matches = cli.clone().get_matches();
-
-    let address = matches.get_one::<String>("address").unwrap();
-    let address = SocketAddr::from_str(address).unwrap();
-
-    let retries = matches
-        .get_one::<String>("retries")
-        .unwrap()
-        .parse()
-        .unwrap();
-
-    match matches.subcommand() {
-        Some(("health", _)) => {
+    match &cli.command {
+        Commands::Enqueue { url } => {
             connect_with_backoff(retries, address, async move |_, mut client| {
-                let request = Request::new(HealthRequest {});
-                let response = client.health(request).await?;
-
-                println!("Health: {:?}", response.into_inner());
-
-                Ok(())
-            })
-            .await?;
-        }
-        Some(("real-time-status", _)) => {
-            connect_with_backoff(retries, address, async move |_, mut client| {
-                loop {
-                    let request = Request::new(RealTimeStatusRequest {});
-                    let response = client.real_time_status(request).await?;
-                    println!("Status: {:#?}", response.into_inner());
-                }
-            })
-            .await?;
-        }
-        Some(("enqueue", enqueue)) => {
-            connect_with_backoff(retries, address, async move |_, mut client| {
-                let url = enqueue.get_one::<String>("url").unwrap().clone();
+                let url = url.to_string();
 
                 let request = Request::new(EnqueueRequest { url });
 
@@ -185,15 +154,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             })
             .await?;
         }
-        Some(("search", search)) => {
+        Commands::Search { words } => {
             connect_with_backoff(retries, address, async move |_, mut client| {
-                let words: Vec<String> = search
-                    .get_one::<String>("words")
-                    .unwrap()
-                    .split_whitespace()
-                    .filter(|word| !word.is_empty())
-                    .map(|string| string.to_string())
-                    .collect();
+                let words = words.iter().filter(|w| !w.is_empty()).cloned().collect();
 
                 let request = Request::new(SearchRequest { words });
 
@@ -205,12 +168,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             })
             .await?;
         }
-        Some(("consult", sub_m)) => match sub_m.subcommand() {
-            Some(("backlinks", backlinks)) => {
+        Commands::Consult { consult_command } => match consult_command {
+            ConsultCommand::Backlinks { url } => {
                 connect_with_backoff(retries, address, async move |_, mut client| {
-                    let url = backlinks.get_one::<String>("url").unwrap().clone();
+                    let url = url.clone();
 
-                    let request = Request::new(BacklinksRequest { url: url.clone() });
+                    let request = Request::new(BacklinksRequest {
+                        url: url.to_string(),
+                    });
 
                     let response = client.consult_backlinks(request).await?.into_inner();
 
@@ -220,11 +185,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 })
                 .await?;
             }
-            Some(("outlinks", outlinks)) => {
+            ConsultCommand::Outlinks { url } => {
                 connect_with_backoff(retries, address, async move |_, mut client| {
-                    let url = outlinks.get_one::<String>("url").unwrap().clone();
+                    let url = url.clone();
 
-                    let request = Request::new(OutlinksRequest { url: url.clone() });
+                    let request = Request::new(OutlinksRequest {
+                        url: url.to_string(),
+                    });
 
                     let response = client.consult_outlinks(request).await?.into_inner();
 
@@ -234,16 +201,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 })
                 .await?;
             }
-            _ => {
-                error!("Invalid consult subcommand");
-                cli.clone().print_help().unwrap();
-            }
         },
-        _ => {
-            error!("Invalid subcommand");
-            cli.clone().print_help().unwrap();
+        Commands::RealTimeStatus => {
+            connect_with_backoff(retries, address, async move |_, mut client| {
+                loop {
+                    let request = Request::new(RealTimeStatusRequest {});
+                    let response = client.real_time_status(request).await?;
+                    println!("Status: {:#?}", response.into_inner());
+                }
+            })
+            .await?;
         }
-    }
+        Commands::Health => {
+            connect_with_backoff(retries, address, async move |_, mut client| {
+                let request = Request::new(HealthRequest {});
+                let response = client.health(request).await?;
+
+                println!("Health: {:?}", response.into_inner());
+
+                Ok(())
+            })
+            .await?;
+        }
+    };
 
     Ok(())
 }
