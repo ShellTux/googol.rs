4db717601c9cbacc4025665b37552592375eae35
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-06-09 04:01:07 +0100

    chore(bench): add title to all benches

--- benches/top_searches_benchmark.rs
+++ benches/top_searches_benchmark.rs
@@ -19,21 +19,24 @@ fn generate_random_word(len: usize) -> String {
 fn benchmark_add_search(c: &mut Criterion) {
     let total_adds = 100_000;
 
-    c.bench_function("add_search 100000 times", |b| {
-        b.iter(|| {
-            // Clear the searches for each iteration if needed
-            // or create a new instance outside the loop
-            let mut local_searches = TopSearches::new();
+    c.bench_function(
+        &format!("top_searches add_search {} times", total_adds),
+        |b| {
+            b.iter(|| {
+                // Clear the searches for each iteration if needed
+                // or create a new instance outside the loop
+                let mut local_searches = TopSearches::new();
 
-            for _ in 0..total_adds {
-                let word = generate_random_word(5);
-                local_searches.add_search(&word);
-            }
+                for _ in 0..total_adds {
+                    let word = generate_random_word(5);
+                    local_searches.add_search(&word);
+                }
 
-            // Use black_box to prevent optimization
-            hint::black_box(local_searches);
-        });
-    });
+                // Use black_box to prevent optimization
+                hint::black_box(local_searches);
+            });
+        },
+    );
 }
 
 fn benchmark_count(c: &mut Criterion) {
@@ -49,9 +52,10 @@ fn benchmark_count(c: &mut Criterion) {
     }
 
     // Pick some words to test count
-    let test_words: Vec<&String> = words.choose_multiple(&mut rng(), 100).collect();
+    let total_words = 100;
+    let test_words: Vec<&String> = words.choose_multiple(&mut rng(), total_words).collect();
 
-    c.bench_function("count 100 words", |b| {
+    c.bench_function(&format!("top_searches count {} words", total_words), |b| {
         b.iter(|| {
             for &word in &test_words {
                 let _count = searches.count(word);
@@ -81,13 +85,16 @@ fn benchmark_top_n(c: &mut Criterion) {
     let top_n = 10;
 
     // Benchmark the top_n method
-    c.bench_function("top_n 10 large dataset", |b| {
-        b.iter(|| {
-            let result = searches.top_n(top_n);
-            // Optionally, do something with result to prevent compiler optimizations
-            hint::black_box(result);
-        });
-    });
+    c.bench_function(
+        &format!("top_searches top_n {} large dataset", top_n),
+        |b| {
+            b.iter(|| {
+                let result = searches.top_n(top_n);
+                // Optionally, do something with result to prevent compiler optimizations
+                hint::black_box(result);
+            });
+        },
+    );
 }
 
 criterion_group!(
